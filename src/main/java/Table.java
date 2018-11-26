5d3d3d025ba7dc1aa54aab1a24eb2e067eeb0d80
Robin230994 <rdort@htwsaar.de>
2018-11-26 16:59:40 +0100

    added javadoc comments in the Table class

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -5,6 +5,10 @@ public class Table {
     private int loc; //Lines of content;
 
 
+    /**
+     * constructors
+     * class sets the head of our medicine file into the head array and the content columns of our file into the content array 
+     */
     public Table(){}
 
     public Table(String[] head, String[][] content, int loc) {
@@ -13,22 +17,43 @@ public class Table {
         this.loc = loc;
     }
 
+    /**
+     * get method
+     * @return the head of our table
+     */
     public String[] getHead(){
         return this.head;
     }
 
+    /**
+     * get method
+     * @return the content of our table
+     */
     public String[][] getContent(){
         return this.content;
     }
 
+    /**
+     * set method
+     * @param content the content that will be set
+     */
     public void setContent(String[][] content){
         this.content = content;
     }
 
+    /**
+     * set method
+     * @param head the head that will be set
+     */
     public void setHead(String[] head){
         this.head = head;
     }
 
+    /**
+     * get method
+     * @param row the row you want to get
+     * @return the given row
+     */
     public String[] getRow(int row){
         String[] arr;
         //arr = this.content[row];
